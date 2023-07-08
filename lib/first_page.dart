79223f6ebdcd8db44c5b0df0f122ae136ec6ea80
Abdul Samad <116899821+abdulsamad08@users.noreply.github.com>
2023-07-08 20:25:49 +0500

    updates

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -17,27 +17,112 @@ class FirstPage extends StatefulWidget {
 class _FirstPageState extends State<FirstPage> {
   final picker = ImagePicker();
   final pdf = pw.Document();
-  List<File> _images = [];
-  
-    File? _imageFile;
-  Future<void> _takePicture() async {
-    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+  List<File> images = [];
+
+  File? _imageFile;
+
+ Future<void> saveToGallery(File imageFile) async {
+  final result = await ImageGallerySaver.saveFile(imageFile.path);
+  debugPrint('Image saved to gallery: $result');
+}
+
+
+  openGallery() async {
+    final pickedImages = await ImagePicker().pickMultiImage(
+      maxWidth: 800,
+      imageQuality: 80,
+    );
+
+    // ignore: unnecessary_null_comparison
+    if (pickedImages == null) return;
 
     setState(() {
-      if (pickedFile != null) {
-        _imageFile = File(pickedFile.path);
-      } else {
-        print('No image selected.');
+      images.clear();
+      for (var image in pickedImages) {
+        images.add(File(image.path));
       }
     });
   }
 
-  Future<void> _saveToGallery() async {
-    if (_imageFile != null) {
-      final result = await ImageGallerySaver.saveFile(_imageFile!.path);
-      print('Image saved to gallery: $result');
+  openCamera() async {
+  final pickedImage = await ImagePicker().pickImage(
+    source: ImageSource.camera,
+    maxWidth: 800,
+    imageQuality: 80,
+  );
+
+  if (pickedImage == null) return;
+
+  final imageFile = File(pickedImage.path);
+  setState(() {
+    images.add(imageFile);
+  });
+
+  saveToGallery(imageFile); // Save the captured image to the gallery
+}
+
+
+  createPDF() async {
+    if (images.isEmpty) {
+      return;
+    }
+
+    for (var imageFile in images) {
+      final image = pw.MemoryImage(imageFile.readAsBytesSync());
+      pdf.addPage(
+        pw.Page(
+          pageFormat: PdfPageFormat.a4,
+          build: (pw.Context context) {
+            return pw.Center(child: pw.Image(image));
+          },
+        ),
+      );
     }
   }
+
+  savePDF() async {
+    if (images.isEmpty) {
+      debugPrint("No images to save as PDF");
+      return;
+    }
+
+    try {
+      final pdf = pw.Document();
+
+      for (var imageFile in images) {
+        final image = pw.MemoryImage(imageFile.readAsBytesSync());
+        pdf.addPage(
+          pw.Page(
+            pageFormat: PdfPageFormat.a4,
+            build: (pw.Context context) {
+              return pw.Center(child: pw.Image(image));
+            },
+          ),
+        );
+      }
+
+      final dir = await getExternalStorageDirectory();
+      final file = File("${dir?.path}/filename.pdf");
+      await file.writeAsBytes(await pdf.save());
+      shodebugPPrintedMessage('Success',
+          'Saved to documents android folder then data folder pdf format');
+    } catch (e) {
+      shodebugPPrintedMessage('Error', e.toString());
+    }
+  }
+
+  shodebugPPrintedMessage(String title, String msg) {
+    Flushbar(
+      title: title,
+      message: msg,
+      duration: const Duration(seconds: 3),
+      icon: const Icon(
+        Icons.info,
+        color: Colors.blue,
+      ),
+    ).show(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,11 +146,11 @@ class _FirstPageState extends State<FirstPage> {
           ),
         ],
       ),
-      body: _images.isNotEmpty
+      body: images.isNotEmpty
           ? ListView.builder(
-              itemCount: _images.length,
+              itemCount: images.length,
               itemBuilder: (context, index) {
-                final image = _images[index];
+                final image = images[index];
                 return Container(
                   height: 400,
                   width: double.infinity,
@@ -79,97 +164,5 @@ class _FirstPageState extends State<FirstPage> {
             )
           : Container(),
     );
-  }
-
-  openGallery() async {
-    final pickedImages = await ImagePicker().pickMultiImage(
-      maxWidth: 800,
-      imageQuality: 80,
-    );
-
-    if (pickedImages == null) return;
-
-    setState(() {
-      _images.clear();
-      for (var image in pickedImages) {
-        _images.add(File(image.path));
-      }
-    });
-  }
-
-  openCamera() async {
-    final pickedImage = await ImagePicker().pickImage(
-      source: ImageSource.camera,
-      maxWidth: 800,
-      imageQuality: 80,
-    );
-
-    if (pickedImage == null) return;
-
-    final imageFile = File(pickedImage.path);
-    setState(() {
-      _images.add(imageFile);
-    });
-  }
-
-  createPDF() async {
-    if (_images.isEmpty) {
-      return;
-    }
-
-    for (var imageFile in _images) {
-      final image = pw.MemoryImage(imageFile.readAsBytesSync());
-      pdf.addPage(
-        pw.Page(
-          pageFormat: PdfPageFormat.a4,
-          build: (pw.Context context) {
-            return pw.Center(child: pw.Image(image));
-          },
-        ),
-      );
-    }
-  }
-
-  savePDF() async {
-    if (_images.isEmpty) {
-      print("No images to save as PDF");
-      return;
-    }
-
-    try {
-      final pdf = pw.Document();
-
-      for (var imageFile in _images) {
-        final image = pw.MemoryImage(imageFile.readAsBytesSync());
-        pdf.addPage(
-          pw.Page(
-            pageFormat: PdfPageFormat.a4,
-            build: (pw.Context context) {
-              return pw.Center(child: pw.Image(image));
-            },
-          ),
-        );
-      }
-
-      final dir = await getExternalStorageDirectory();
-      final file = File("${dir?.path}/filename.pdf");
-      await file.writeAsBytes(await pdf.save());
-      showPrintedMessage('Success', 'Saved to documents android folder then data folder pdf format');
-    } catch (e) {
-     
-      showPrintedMessage('Error', e.toString());
-    }
-  }
-
-  showPrintedMessage(String title, String msg) {
-    Flushbar(
-      title: title,
-      message: msg,
-      duration: const Duration(seconds: 3),
-      icon: const Icon(
-        Icons.info,
-        color: Colors.blue,
-      ),
-    ).show(context);
   }
 }
